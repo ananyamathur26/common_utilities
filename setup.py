8b46dd82824b216ee8d5bfe136892f0d642cd517
ananyamathur26 <ananya.mathur@digicollect.com>
2024-02-29 04:01:34 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='common_utilities',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:ananyamathur26/common_utilities-sdk-package.git',
+    author='Ananya Mathur',
+    author_email='ananyamathur26@gmail.com',
+    license='unlicense',
+    packages=['common_utilities'],
+    zip_safe=False
+)
+
