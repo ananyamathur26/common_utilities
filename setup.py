c81cc26aae4e3ada50d472b76bfe0a5b1cde10e2
ananyamathur26 <ananya.mathur@digicollect.com>
2024-02-29 07:04:01 +0530

    #fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='common_utilities',
     version='0.0.1',
     description='My private package from private github repo',
-    url='git@github.com:ananyamathur26/common_utilities.git',
+    url='https://github.com/ananyamathur26/common_utilities.git',
     author='Ananya Mathur',
     author_email='ananyamathur26@gmail.com',
     license='unlicense',
