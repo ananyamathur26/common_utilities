58ef3923abd332cbbd6592809a9e0f4b834abec1
ananyamathur26 <ananya.mathur@digicollect.com>
2024-02-29 07:00:09 +0530

    #fix

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='common_utilities',
     version='0.0.1',
     description='My private package from private github repo',
-    url='git@github.com:ananyamathur26/common_utilities-sdk-package.git',
+    url='git@github.com:ananyamathur26/common_utilities.git',
     author='Ananya Mathur',
     author_email='ananyamathur26@gmail.com',
     license='unlicense',
-    packages=['common_utilities'],
+    packages=find_packages(),
     zip_safe=False
 )
 
